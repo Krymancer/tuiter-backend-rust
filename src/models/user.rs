c7947a91fa474adcee28b34b227d7c4ef91a00e9
Júnior Nascimento <junior.nascm@gmail.com>
2023-03-20 23:53:06 -0300

    sync

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,20 +1,28 @@
 use uuid::Uuid;
 use serde::{Serialize, Deserialize};
-
+use chrono::{DateTime, Utc};
+use chrono::serde::ts_seconds::serialize as to_ts;
 
 #[derive (Debug, Serialize, Deserialize)]
 pub struct User {
-    pub id: String,
+    pub id: Uuid,
     pub username: String,
-    pub password: String,
+    pub hash: String,
+    pub icon: String,
+    pub bio: String,
+    #[serde(serialize_with = "to_ts")]
+    pub created_at: DateTime<Utc>,
 }
 
 impl User {
-    pub fn new(username: String, password: String) -> Self {
+    pub fn new(username: String, password: String) -> Self { 
         Self {
-            id: Uuid::new_v4().to_string(),
+            id: Uuid::new_v4(),
             username,
-            password
+            hash: password,
+            icon: "".to_string(),
+            bio: "".to_string(),
+            created_at: Utc::now(),
         }
     }
 }
--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -2,8 +2,9 @@ pub mod user;
 pub mod tweet;
 pub mod like;
 
-use rusqlite::{Connection, Result};
+use rusqlite::Connection;
+use anyhow::Error;
 
-pub fn get_connection() -> Result<Connection> {
+pub fn get_connection() -> Result<Connection, Error> {
   Ok(Connection::open("tweet.db")?)
 }
