841a638245fce2d92159fa614fca997db702a5f0
Júnior Nascimento <junior.nascm@gmail.com>
2023-04-01 02:35:31 -0300

    Change rusqlite to sqlx

--- src/routes/tweet.rs
+++ src/routes/tweet.rs
@@ -70,9 +70,11 @@ pub async fn get_tweets(context: State<ApiContext>) -> Response {
             User.bio AS "author_bio!", 
             User.icon AS "author_icon!",
             User.created_at AS "author_created_at!",
-            0 AS "likes!"
+            COUNT(Likes.tweet) AS "likes!"
             FROM Tweet 
             JOIN User ON Tweet.author = User.id
+            LEFT JOIN Likes ON Tweet.id = Likes.tweet
+            GROUP BY Tweet.id
         "#,
     )
     .fetch_all(&context.database)
@@ -91,11 +93,86 @@ pub async fn get_tweets(context: State<ApiContext>) -> Response {
     }))).into_response()
 }
 
-pub async fn delete_tweet(Path(id) : Path<String>) -> Result<Json<Value>, StatusCode> {
-    Ok(Json(json!({ "path": "/tweet/:id", "method": "DELETE", "id": id})))
+pub async fn delete_tweet(
+    auth_user: AuthUser,
+    context: State<ApiContext>,
+    Path(id) : Path<String>
+) -> Response {
+    let author_id = auth_user.user_id.to_string();
+
+    let tweet = sqlx::query_scalar!(
+        r#"
+        SELECT
+            Tweet.id AS "id!"
+        FROM Tweet
+        WHERE Tweet.id = $1 AND Tweet.author = $2
+        "#,
+        id, author_id
+        )
+        .fetch_one(&context.database)
+        .await;
+    
+    let tweet = match tweet {
+        Ok(tweet) => tweet,
+        Err(_) => return (StatusCode::BAD_REQUEST, Json(json!({
+            "message": "cannot find this tweet from this user in database"
+        }))).into_response()
+    };
+
+    sqlx::query!(
+        r#"
+        DELETE FROM Tweet WHERE id = $1
+        "#,
+        id,
+        )
+        .execute(&context.database)
+        .await
+        .expect("failed to delete tweet");
+
+    (StatusCode::OK, Json(json!({
+        "message": "tweet deleted",
+        "id": tweet
+    }))).into_response()
 } 
  
 
-pub async fn get_tweet(Path(id) : Path<String>) -> Result<Json<Value>, StatusCode> {
-    Ok(Json(json!({ "path": "/tweet/:id", "method": "GET", "id": id})))
+pub async fn get_tweet(
+    context: State<ApiContext>,
+    Path(id) : Path<String>
+) -> Response {
+    let tweet = sqlx::query_as!(
+        TweetQuery,
+        r#"
+        SELECT
+            Tweet.id AS "id!", 
+            Tweet.content AS "content!",
+            Tweet.created_at AS "created_at!",
+            User.id AS "author_id!",
+            User.username AS "author_username!", 
+            User.hash AS "author_hash!",
+            User.bio AS "author_bio!", 
+            User.icon AS "author_icon!",
+            User.created_at AS "author_created_at!",
+            0 AS "likes"
+            FROM Tweet 
+            JOIN User ON Tweet.author = User.id
+            LEFT JOIN Likes ON Tweet.id = Likes.tweet
+            WHERE Tweet.id = $1
+            GROUP BY Tweet.id
+        "#,
+        id
+    )
+    .fetch_one(&context.database)
+    .await;
+    
+    let tweet = match tweet {
+        Ok(tweet) => tweet,
+        Err(_) => return (StatusCode::BAD_REQUEST, Json(json!({
+            "message": "Cloud not find tweet with id provided"
+        }))).into_response()
+    };
+
+    (StatusCode::OK, Json(json!({
+        "tweet": tweet
+    }))).into_response()
 }
